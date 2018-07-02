4826fae9494882e5101352a62a44a5264c86de56
maoxiaomai <416344204@qq.com>
2018-07-02 23:51:54 +0800

    对10个数进行排序

--- Py100/037.py
+++ Py100/037.py
@@ -6,7 +6,15 @@ for i in range(0, 10):
     num = int(input("input a number: "))
     L.append(num)
 
-# print(L)
-L.sort()
-for i in L:
-    print(i, end=' ')
+print(L)
+# L.sort()
+# for i in L:
+#     print(i, end=' ')
+
+# 冒泡排序
+
+for i in range(0, len(L)):
+    for j in range(i+1, len(L)):
+        if L[i] > L[j]:
+            L[i], L[j] = L[j], L[i]
+print(L)
