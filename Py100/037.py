83a8bc66b5397ecd1452559daa84fcbf3ff8478b
maoxiaomai <416344204@qq.com>
2018-07-03 15:02:01 +0800

    对10个数进行排序。冒泡算法

--- Py100/037.py
+++ Py100/037.py
@@ -1,20 +1,35 @@
 # -*- coding: utf-8 -*-
 # 题目：对10个数进行排序。
 
-L = []
-for i in range(0, 10):
-    num = int(input("input a number: "))
-    L.append(num)
+#L=[3, 55, 28, 91, 25, 87, 44]
+# print(L)
+# # L.sort()
+# # for i in L:
+# #     print(i, end=' ')
+#
+#
+#
+# count = 0
+# for i in range(0, len(L)-1):
+#     for j in range(i+1, len(L)):
+#         if L[i] > L[j]:
+#             L[i], L[j] = L[j], L[i]
+#         count += 1
+#     print(L)
+# print(count)
 
-print(L)
-# L.sort()
-# for i in L:
-#     print(i, end=' ')
 
 # 冒泡排序
+def bubble_sort(nums):
+    #count = 0
+    for i in range(0, len(nums)-1):
+        for j in range(0, len(nums)-1-i):
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+            count += 1
+        print(nums)
+    #print(count)
+    return nums
 
-for i in range(0, len(L)):
-    for j in range(i+1, len(L)):
-        if L[i] > L[j]:
-            L[i], L[j] = L[j], L[i]
-print(L)
+nums=[3, 55, 28, 91, 25, 87, 44]
+bubble_sort(nums)
