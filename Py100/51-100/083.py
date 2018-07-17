8b1b145513cd5c84e2b0cfe10e1f0262407057c7
maoxiaomai <416344204@qq.com>
2018-07-17 15:41:57 +0800

    join()

---
+++ Py100/51-100/083.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# 连接字符串。
+
+# Python join() 方法用于将序列中的元素以指定的字符连接生成一个新的字符串。
+
+delimiter = ','
+mylist = ['Brazil', 'Russia', 'India', 'China']
+
+print(delimiter.join(mylist))
+
+
+
+
